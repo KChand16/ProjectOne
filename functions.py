0b8f84cbd1ee203daf26c9198e3526405e2d1533
Krishen Chand <krishenchand@Krishens-MacBook.local>
2020-05-10 11:23:49 -0700

    Test

---
+++ functions.py
@@ -0,0 +1,8 @@
+def average(numbers):
+    length = len(numbers)
+    total = 0.0
+    for number in numbers:
+        total = total + number
+    return total/length
+print (average([1,5,9]))
+print (average(range(11)))
